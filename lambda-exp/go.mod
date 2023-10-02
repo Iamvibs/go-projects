ec6308f6240f1db18ace2039fff2cc0c37176040
iamvibs <vm11082001@gmail.com>
2023-10-02 17:08:12 +0530

    aws lambda with go

---
+++ lambda-exp/go.mod
@@ -0,0 +1,5 @@
+module lambda-func
+
+go 1.21.1
+
+require github.com/aws/aws-lambda-go v1.41.0
---
+++ lambda-exp/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type MyEvent struct {
+	Name string `json:"what is your name?"`
+	Age  int    `json:"how old are you?"`
+}
+
+type MyResponse struct {
+	Message string `json:"answer:"`
+}
+
+func HandleLambdaEvent(event MyEvent) (MyResponse, error) {
+	return MyResponse{Message: fmt.Sprintf("%s is %d years old!", event.Name, event.Age)}, nil
+}
+
+func main() {
+
+	lambda.Start(HandleLambdaEvent)
+}
